b3b0a02ea34cbb121a520ab77bb6db6242f5dfbb
FACU CUENCA <117240275+facucuenca@users.noreply.github.com>
2023-04-14 02:38:07 -0300

    implementando experiencia al backend error

---
+++ src/main/java/com/proyecto/Back/End/Controller/CExperiencia.java
@@ -0,0 +1,30 @@
+
+package com.proyecto.Back.End.Controller;
+
+import com.proyecto.Back.End.Entity.Experiencia;
+import com.proyecto.Back.End.Service.SExperiencia;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("explab")
+@CrossOrigin(origins = "http://localhost:4200")
+public class CExperiencia {
+    @Autowired
+    SExperiencia sExperiencia;
+    
+    @GetMapping("/lista")
+    public ResponseEntity<List<Experiencia>> list(){
+        List<Experiencia> list = sExperiencia.list();
+        return new ResponseEntity(list, HttpStatus.OK);
+    }
+    
+
+    
+}
